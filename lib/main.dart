85bc02a55ec32341848fe878eab7553aa0fc743c
Artem <timsanru39@gmail.com>
2023-01-16 17:02:28 +0200

    add text

--- lib/main.dart
+++ lib/main.dart
@@ -95,11 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              'Приложение для зубных техников!!!',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
